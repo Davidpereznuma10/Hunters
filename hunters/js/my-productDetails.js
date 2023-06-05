ab873b2d47dd111a3b9a27558d0d576e28cce21b
David Pérez Numa <122552606+Davidpereznuma10@users.noreply.github.com>
2023-06-05 15:50:12 -0500

    feat: :sparkles: Add the correct file

---
+++ hunters/js/my-productDetails.js
@@ -0,0 +1,45 @@
+/**
+ * todo: Miguel del futuro arregle esto ▼ 
+ * ? Esta una Mier.....da :)
+ * @var styles
+ */
+import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
+export class myBody extends HTMLElement {
+    constructor() {
+        super();
+        this.count = 1;
+        document.adoptedStyleSheets.push(styles);
+    }
+    async components() {
+        return await (await fetch("view/my-body.html")).text();
+    }
+    async add(e){
+        /**
+         * todo: Miguel del futuro arregle esto ▼ 
+         * ? Arreglar esto se duplica :(
+         * 
+         */
+        let $ = e.target;
+        if ($.nodeName == "BUTTON") {
+            let plantilla = await (await fetch("view/my-productDetails.html")).text();
+            let render = new DOMParser().parseFromString(plantilla, "text/html")["body"];
+            render.children[0].id="product_"+this.count;
+            let buttons = render.querySelectorAll("button");
+            buttons.forEach(element => {
+                element.dataset.row = "product_"+this.count;
+            });
+            let div = document.createElement("my-product-details");
+            div.insertAdjacentElement("beforeend", render.children[0]);
+            document.querySelector("#products").insertAdjacentElement("beforeend", div);
+            this.count++;
+        }
+    }
+    connectedCallback() {
+        this.components().then(html => {
+            this.innerHTML = html;
+            this.add = this.querySelector("#add").addEventListener("click", this.add.bind(this));
+         
+        })
+    }
+}
+customElements.define('my-body', myBody);
